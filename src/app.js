260b153b3a987836edcb8ddb7d0abb72c22b80b8
vedprakash2017 <vedprakashtawar2016@gmail.com>
2022-01-21 05:16:53 +0530

    small

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const forecast = require('./utils/forecast.js')
 const publicdir = path.join(__dirname , '../public')
 const viewPath = path.join(__dirname , '../templates/views')
 const partialPath = path.join(__dirname , '../templates/partials')
-const port = process.env.PORT | 4000
+const port = process.env.PORT || 3000
 
 app.use(express.static(publicdir))
 app.set('view engine' , 'hbs')
