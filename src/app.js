85b0c224a007a7dcdb0e614fb7c1a5ec0c693654
vedprakash2017 <vedprakashtawar2016@gmail.com>
2022-01-21 05:10:01 +0530

    setup

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ const forecast = require('./utils/forecast.js')
 const publicdir = path.join(__dirname , '../public')
 const viewPath = path.join(__dirname , '../templates/views')
 const partialPath = path.join(__dirname , '../templates/partials')
+const port = process.env.PORT | 4000
 
 app.use(express.static(publicdir))
 app.set('view engine' , 'hbs')
@@ -89,6 +90,6 @@ app.get('*' , (req,res)=>{
         error:'Page not found'
     })
 })
-app.listen(3000 , ()=>{
+app.listen( port , ()=>{
     console.log('Server is running!')
 })
